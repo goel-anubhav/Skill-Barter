7817d4cdc4f948005b8e5ae41e06235b00816e42
unknown <anubhavgoyal12062001@gmail.com>
2024-07-23 15:01:46 +0530

    Frotend done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,82 @@
-import 'semantic-ui-css/semantic.min.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './App.css'
+import Jobs from './components/Jobs'
+import Home from './components/Home'
+import JobDescription from './components/JobDescription'
+import Login from './components/auth/Login'
+import Singup from './components/auth/Singup'
+import Profile from './components/Profile'
+import PostJob from './components/PostedJobs'
+import Companies from './components/Companies'
+import CompanySetup from './components/CompanySetup'
+import CompanyCreate from './components/CompanyCreate'
+import Browse from './components/Browse'
+import CreateJobs from './components/admin/CreateJobs'
+import ProtectedRoute from './components/admin/ProtectedRoute'
+import Applicants from './components/admin/Applicants'
 
-import Signup from './Components/Signup';
-import Login from './Components/Login';
-import LandingPage from './Components/landingpage'; // Update the import to use consistent casing
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "/jobs",
+    element: <Jobs />
+  },
+  {
+    path: "/description/:id",
+    element: <JobDescription />
+  },
+  {
+    path: "/browse",
+    element: <Browse />
+  },
+  {
+    path: "/login",
+    element: <Login />
+  },
+  {
+    path: "/signup",
+    element: <Singup />
+  },
+  {
+    path:"/profile",
+    element: <Profile/>
+  },
+  // Admin Dashboard Route Started
+  {
+    path:"/admin/jobs",
+    element:<ProtectedRoute><PostJob/></ProtectedRoute> 
+  },
+  {
+    path:"/admin/jobs/create",
+    element:<ProtectedRoute><CreateJobs/></ProtectedRoute> 
+  },
+  {
+    path:"/admin/jobs/:id/applicants",
+    element:<ProtectedRoute><Applicants/></ProtectedRoute> 
+  },
+  {
+    path:"/admin/companies",
+    element:<ProtectedRoute><Companies/></ProtectedRoute>
+  },
+  {
+    path:"/admin/companies/create",
+    element:<ProtectedRoute><CompanyCreate/></ProtectedRoute> 
+  },
+  {
+    path:"/admin/companies/:id",
+    element:<ProtectedRoute><CompanySetup/></ProtectedRoute> 
+  },
+])
 
 function App() {
   return (
-    <>
-      <LandingPage /> {/* Update the component name to use PascalCase */}
-    </>
-  );
+    <div>
+      <RouterProvider router={appRouter} />
+    </div>
+  )
 }
 
-export default App;
+export default App
