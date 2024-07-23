a1c227e412c9a8a7107932353beb5e97a3d9759f
unknown <anubhavgoyal12062001@gmail.com>
2024-07-23 11:31:44 +0530

    Landing Page Created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,17 +2,14 @@ import 'semantic-ui-css/semantic.min.css';
 
 import Signup from './Components/Signup';
 import Login from './Components/Login';
-
+import LandingPage from './Components/landingpage'; // Update the import to use consistent casing
 
 function App() {
-
-
   return (
     <>
-    <Signup/>
-     
+      <LandingPage /> {/* Update the component name to use PascalCase */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
